b5fc24dd0a6338fc36a47d00acab0d49152ded6b
Mart-V-V <pretpiro@gmail.com>
2022-10-23 18:18:06 +0300

    fetch_v3

--- src/components/ListPost.js
+++ src/components/ListPost.js
@@ -36,30 +36,55 @@
 
 
 import React from "react";
-import Dates from "../myfile.json";
+// import Dates from "../myfile.json";
 import { Link } from "react-router-dom";
 
 
 
-  const apiUrl = 'http://ussr-coins.ru/tphpjson/myfile.json';
-  fetch(apiUrl, { mode: 'no-cors',     method: 'POST',
-  headers: { 'Content-Type': 'application/json' }, })
+  // const apiUrl = 'http://ussr-coins.ru/tphpjson/myfile.json';
+  fetch('/manifest.json', 
+  {method: 'GET',
+   mode: 'no-cors',
+   headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+   }
+ })
 
   .then((response) => response.json())
   .then((data) => console.log('This is your data', data));
 
 
-export const ListPost = () => {
-  return (
+  export const ListPost = (props) => {
+    const { repos } = props;
+    if (!repos || repos.length === 0) return <p>No repos, sorry</p>;
+    return (
       <ul>
-      {Dates?.map(date => (
-        <li key={date.ID}>
-            <Link to={`/post/${date.ID}`}>
-                {date.post_title}
-            </Link>
-            {date.post_content}
-        </li>
-      ))}
+        <h2 className='list-head'>Available Public Repositories</h2>
+        {repos.map((repo) => {
+            <li key={repo.ID} className='list'>
+              <span className='repo-text'>{repo.post_title} </span>
+              <span className='repo-description'>{repo.post_content}</span>
+            </li>
+        })}
       </ul>
-  );
-}
+    );
+  };
+  // export default ListPost;
+
+
+// export const ListPost = () => {
+//   return (
+//       <ul>
+//       {Dates?.map(date => (
+//         <li key={date.ID}>
+//             <Link to={`/post/${date.ID}`}>
+//                 {date.post_title}
+//             </Link>
+//             {date.post_content}
+//         </li>
+//       ))}
+//       </ul>
+//   );
+// }
