ada289027975fbb3ce05711f05cad08db5447746
martyanov.vladimir <martyanov.vladimir@uep24.ru>
2022-10-31 12:57:57 +0300

    add post one

--- src/components/PostOne.js
+++ src/components/PostOne.js
@@ -5,62 +5,23 @@ import { useParams } from 'react-router-dom';
 export const PostOne = () => {
   const [users, setUsers] = useState([])
   const postId = useParams().id;
-
+  const apiUrl = 'http://ussr-coins.ru/api/product/read_one.php?id=' + postId;
 
   const fetchData = async () => {
-    const response = await fetch('http://ussr-coins.ru/api/product/read_one.php?id=${postId}')
+    const response = await fetch(apiUrl)
     const data = await response.json()
     setUsers(data)
-    console.log(data);
   }
 
   useEffect(() => {
     fetchData()
   }, [])
 
-  const filtered = users(item => {
-    return item.ID === postId;
-  });
-
-// console.log(postId);
 
   return (
     <div>
-    {filtered.map(item => {
-      return (
-        <div key={item.Id}>
-          <h1>{item.name}</h1>
-          <p>{item.description}</p>
-        </div>
-      );
-    })}
+            <h1 key={users.id}>{users.name}</h1>
+            <p>{users.description}</p>
     </div>
   )
 }
-
-
-// import React from "react";
-// import { useParams } from 'react-router-dom';
-// import Dates from "../myfile.json";
-
-// export const PostOne = () => {
-
-  // const postId = useParams().id;
-
-  // const filtered = Dates.filter(item => {
-  //   return item.ID === postId;
-  // });
-
-//   return (
-      // <div>
-      // {filtered.map(item => {
-      //   return (
-      //     <div key={item.ID}>
-      //       <h1>{item.post_title}</h1>
-      //       <p>{item.post_content}</p>
-      //     </div>
-      //   );
-      // })}
-      // </div>
-//   );
-// }
